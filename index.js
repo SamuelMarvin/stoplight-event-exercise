44ac95f8e73eb6b05eee646b00ef49c88ca41f23
Samuel Marvin <samjmarv@gmail.com>
2022-02-04 17:41:18 -0600

    first push hanges to be committed:

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-(function() {
-  'use strict';
+const stopB = document.getElementById('stopButton');
+const stopL = document.getElementById('stopLight');
+const slowB = document.getElementById('slowButton');
+const slowL = document.getElementById('slowLight');
+const goB = document.getElementById('goButton');
+const goL = document.getElementById('goLight');
+const controls  = document.querySelectorAll('.controls');
 
-  // YOUR CODE HERE
-})();
+
+controls.forEach( () => controls.addEventListener('click', console.log(`${controls.innerText} bulb on`)))
+
+stopB.addEventListener('click', () => stopL.classList.toggle('stop'))
+stopB.addEventListener('mouseenter', () => console.log('Entered stop button'))
+stopB.addEventListener('mouseleave', () => console.log('Left stop button'))
+
+slowB.addEventListener('click', () => slowL.classList.toggle('slow'))
+slowB.addEventListener('mouseenter', () => console.log('Entered slow button'))
+slowB.addEventListener('mouseleave', () => console.log('Left slow button'))
+
+goB.addEventListener('click', () => goL.classList.toggle('go'))
+goB.addEventListener('mouseenter', () => console.log('Entered go button'))
+goB.addEventListener('mouseleave', () => console.log('Left go button'))
